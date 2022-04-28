09a65fe51f196dd2af6b987b552335611c7bf355
Kobi Hikri <kobi.hikri@gmail.com>
2022-04-28 12:48:56 +0300

    modified demo

--- demo/1-producer-produce-to-topic/src/main/java/Main.java
+++ demo/1-producer-produce-to-topic/src/main/java/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String... args){
+
+        Properties producerProperties = new Properties();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProperties.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        Producer<String, String> kafkaProducer = new KafkaProducer<>(producerProperties);
+
+        ProducerRecord<String, String> message
+                = new ProducerRecord("destination-topic-name","message-key","message-value");
+
+        try{
+            System.out.println("Producing message");
+            Future<RecordMetadata> sendFuture = kafkaProducer.send(message);
+            kafkaProducer.flush();
+            RecordMetadata recordMetadata = sendFuture.get();
+            System.out.println("Message produced: recordMetadata = partition:" + recordMetadata.partition() + ", offset:" + recordMetadata.offset());
+        } catch (Exception ex){
+            System.out.println("Error producing message: exception = " + ex.getMessage());
+        }finally {
+            kafkaProducer.flush();
+            kafkaProducer.close();
+            System.out.println("Producer flushed and closed");
+        }
+    }
 }
