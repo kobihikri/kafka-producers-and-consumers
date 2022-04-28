e31c0fdf1b531b8fc769e65c4439e3280ac110b0
Kobi Hikri <kobi.hikri@gmail.com>
2022-04-28 15:25:43 +0300

    created a sample consumer app

--- demo/2-consumer-consume-from-topic/src/main/java/Main.java
+++ demo/2-consumer-consume-from-topic/src/main/java/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String... args){
+
+        Properties consumerProperties = new Properties();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        consumerProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "KafkaConsumer");
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerProperties);
+
+        kafkaConsumer.subscribe(Collections.singletonList("test-topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.topic() + ":" + record.offset());
+            }
+            try {
+                kafkaConsumer.commitSync();
+            } catch (CommitFailedException e) {
+                System.out.println("commit failed");
+            }
+        }
+
+
+    }
 }
+
